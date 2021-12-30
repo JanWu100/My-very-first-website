e363b0dd09e3290f612acd90655e89ccac37c0cf
JanWu <wagrowskij@gmail.com>
2021-12-30 05:03:15 +0100

    Create Parallax

---
+++ app.js
@@ -0,0 +1,21 @@
+//// PARALLAX //////
+
+window.addEventListener("scroll", function (e) {
+  const targetOne = document.querySelector(".z0");
+  const targetTwo = document.querySelector(".z1");
+
+  const targetThree = document.querySelector(".z2");
+  let scrolled = window.pageYOffset;
+  let rateOne = scrolled * 0.15;
+  let rateTwo = scrolled * -0.02;
+  let rateThree = scrolled * -0.2;
+  let scaleRate = scrolled * 0.0002;
+
+  targetOne.style.transform = `translate3d(0px, ${rateOne}px ,0px)`;
+  targetOne.style.transform = `scale(${1 - scaleRate})`;
+  targetTwo.style.transform = `rotate(${rateTwo}deg)`;
+
+  targetThree.style.transform = `translate3d(0px, ${rateThree}px ,0px)`;
+});
+
+///////////////////////
