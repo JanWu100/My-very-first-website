df0dba702b5bd9d87f1b0779e0446b07daf64e84
JanWu <wagrowskij@gmail.com>
2021-12-31 01:15:10 +0100

    Add opacity effects.

--- js/main.js
+++ js/main.js
@@ -6,14 +6,19 @@ window.addEventListener("scroll", function (e) {
 
   const targetThree = document.querySelector(".z2");
   const targetFour = document.querySelector(".z4");
+  const scrollIcon = this.document.querySelector(".scroll");
   let scrolled = window.pageYOffset;
   let rateOne = scrolled * 0.15;
-  let rateTwo = scrolled * -0.02;
+  // let rateTwo = scrolled * -0.02;
   let rateThree = scrolled * -0.2;
   let rotationRate = scrolled * 0.2;
-  let scaleRate = scrolled * 0.0002;
+  let opacityRate = scrolled * 0.0015;
+  // let scaleRate = scrolled * 0.0002;
 
   targetOne.style.transform = `translate3d(0px, ${rateOne}px ,0px)`;
+  scrollIcon.style.opacity = `${1 - opacityRate}`;
+  targetOne.style.opacity = `${1 - opacityRate}`;
+
   //targetOne.style.transform = `scale(${1 - scaleRate})`;
   //targetTwo.style.transform = `rotate(${rateTwo}deg)`;
   targetFour.style.transform = `rotate(${rotationRate}deg)`;
