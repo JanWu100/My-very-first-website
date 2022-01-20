d547d767b61a7ba5b9f66b8704902a5abe1cbbb7
JanWu <wagrowskij@gmail.com>
2022-01-20 13:48:09 +0100

    Recreate Hero section with svg's. Tidy up a bit.

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
 //// PARALLAX //////
 
 window.addEventListener("scroll", function (e) {
-  const targetOne = document.querySelector(".z0");
-  const targetTwo = document.querySelector(".z1");
+  const heroBottomLayer = document.querySelector("#dots");
+  const heroBottomElipse = document.querySelector("#elipse");
 
-  const targetThree = document.querySelector(".z2");
-  const targetFour = document.querySelector(".z4");
+  const heroTopLayer = document.querySelector("#top");
+  const heroCross = document.querySelector("#cross");
   const scrollIcon = this.document.querySelector(".scroll");
   let scrolled = window.pageYOffset;
   let rateOne = scrolled * 0.15;
-  // let rateTwo = scrolled * -0.02;
-  let rateThree = scrolled * -0.2;
+  
+  let rateThree = scrolled * -0.15;
   let rotationRate = scrolled * 0.2;
   let opacityRate = scrolled * 0.0015;
-  // let scaleRate = scrolled * 0.0002;
-
-  targetOne.style.transform = `translate3d(0px, ${rateOne}px ,0px)`;
+ 
   scrollIcon.style.opacity = `${1 - opacityRate}`;
-  targetOne.style.opacity = `${1 - opacityRate}`;
 
-  //targetOne.style.transform = `scale(${1 - scaleRate})`;
-  //targetTwo.style.transform = `rotate(${rateTwo}deg)`;
-  targetFour.style.transform = `rotate(${rotationRate}deg)`;
-  targetThree.style.transform = `translate3d(0px, ${rateThree}px ,0px)`;
+  heroBottomLayer.style.transform = `translate3d(0px, ${rateOne}px ,0px)`;
+  heroBottomLayer.style.opacity = `${1 - opacityRate}`;
+  heroBottomElipse.style.transform = `translate3d(0px, ${rateOne}px ,0px)`;
+
+
+  heroCross.style.transform = `rotate(${rotationRate}deg)`;
+  heroTopLayer.style.transform = `translate3d(0px, ${rateThree}px ,0px)`;
 });
 
 ///////////////////////
